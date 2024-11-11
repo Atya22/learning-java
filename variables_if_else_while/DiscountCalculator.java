bbcce84e041224a705aec8190796c00a8368d5e0
Atya22 <aytaj2002eva@gmail.com>
2024-11-11 14:53:21 +0400

    Add description

--- variables_if_else_while/DiscountCalculator.java
+++ variables_if_else_while/DiscountCalculator.java
@@ -1,5 +1,18 @@
 import java.util.Scanner;
 
+/**
+ * DiscountCalculator is a simple program that calculates a discount
+ * based on the initial purchase amount entered by the user.
+ * 
+ * The discount is calculated in such a way that for every increment
+ * of purchase amount exceeding a base of 300 AZN, an additional 5%
+ * discount is added. The rate increases as long as the adjusted 
+ * amount is greater than 300.
+ * 
+ * The program outputs both the total payable amount after applying 
+ * the discount and the amount of the discount itself.
+ */
+
 public class DiscountCalculator {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
