05e79316e66bb2e0ce587301202aa22b55a968a8
Atya22 <aytaj2002eva@gmail.com>
2024-11-15 01:26:58 +0400

    Add digit counter

---
+++ variables_if_else_while/CountDigits.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        int digitCount = 0;
+
+        if (number == 0) {
+            digitCount = 1;
+        } else {
+            while (number > 0) {
+                number /= 10;
+                digitCount++;
+            }
+            }
+        System.out.println("Amount of numbers: " + digitCount);
+        }
+    }
