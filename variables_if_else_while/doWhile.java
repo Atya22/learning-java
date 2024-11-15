41d72f9ad20c6bc86150d12b49ad050aa2fc1c7a
Atya22 <aytaj2002eva@gmail.com>
2024-11-16 01:17:17 +0400

    Add file

---
+++ variables_if_else_while/doWhile.java
@@ -0,0 +1,14 @@
+public class doWhile {
+    public static void main(String[] args){
+        int sum = 0;
+        int i = 4;
+        do {
+         sum += i;
+         i --;  
+        }
+        while (i > 0);
+
+        System.out.println(sum);
+    
+    }
+}
