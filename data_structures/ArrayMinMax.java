49e24eabca74853738f85e83e36f56356d3c93f2
Atya22 <aytaj2002eva@gmail.com>
2024-11-18 01:36:03 +0400

    Fix if statement

--- data_structures/ArrayMinMax.java
+++ data_structures/ArrayMinMax.java
@@ -18,7 +18,8 @@ public class ArrayMinMax {
         for (int i = 0; i < arr.length; i++){
             if (arr[i] < min){
                 min = arr[i];
-            } else if (arr[i] > max){
+            }
+            if (arr[i] > max){
                 max = arr[i];
             }
         }
