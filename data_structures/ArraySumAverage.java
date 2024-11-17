2232dd66c77c22daa3685ad1826fffe811359f90
Atya22 <aytaj2002eva@gmail.com>
2024-11-18 02:34:57 +0400

    Fix data types

--- data_structures/ArraySumAverage.java
+++ data_structures/ArraySumAverage.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class ArraySumAverage {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the length of array");
+        System.out.print("Enter the length of array: ");
         int lengthArr = sc.nextInt();
-        int[] arr = new int[lengthArr];
-        int sum = 0;
-        int average = 0;
+        float[] arr = new float[lengthArr];
+        float sum = 0;
+        float average;
 
         System.out.println("Enter elements for the array:");
         for (int i = 0; i < arr.length; i++) {
@@ -19,7 +19,7 @@ public class ArraySumAverage {
         }
         average = sum / lengthArr;
 
-        String str = String.format("The sum of the numbers: %s, The average of the numbers: %s", sum, average);
+        String str = String.format("The sum of the numbers: %,.0f, The average of the numbers: %.2f", sum, average);
         System.out.println(str);
     }
 }
