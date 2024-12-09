642a56d0797de7ddce2609a4f0e32b12804696be
Atya22 <aytaj2002eva@gmail.com>
2024-12-10 00:38:25 +0400

    Add getter for the sureName

--- data_structures/HashMap_HashSet/Student.java
+++ data_structures/HashMap_HashSet/Student.java
@@ -19,6 +19,10 @@ public class Student {
         return name;
     }
 
+    public String getSureName() {
+        return sureName;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
